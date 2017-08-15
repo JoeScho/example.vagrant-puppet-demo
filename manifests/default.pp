dc3bb6d6bbd999fec0f95454410857db19f456cb
Joe Schofield <joseph.schofield@yld.io>
2017-08-15 14:52:00 +0100

    Initial commit

---
+++ manifests/default.pp
@@ -0,0 +1,36 @@
+# Execute apt-get update
+exec {'apt-update':
+  command => '/usr/bin/apt-get update'
+}
+
+# Install mongodb package
+package { 'mongodb':
+  require => Exec['apt-update],
+  ensure => installed
+}
+
+# Run service
+service { 'mongodb':
+  ensure => running,
+  require => Package['mongodb']
+}
+
+# Install redis-server package
+package { 'redis-server':
+  require => Exec['apt-update],
+  ensure => installed
+}
+
+# Run service
+service { 'redis-server':
+  ensure => running,
+  require => Package['redis-server']
+}
+
+# Create directory
+file { '/home/vagrant/my-projects':
+  ensure => 'directory',
+  owner => 'vagrant',
+  group => 'vagrant',
+  mode => 750
+}
